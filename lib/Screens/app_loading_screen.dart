b830c3be1a6a88d23c2965f421e8e4d7b63927c9
FeritGV <edirneli34222222@gmail.com>
2025-10-12 17:31:04 +0300

    Firebase integrated

---
+++ lib/Screens/app_loading_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadingState extends ChangeNotifier{
+  
+}
+
+class AppLoadingScreen extends StatelessWidget {
+  const AppLoadingScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}

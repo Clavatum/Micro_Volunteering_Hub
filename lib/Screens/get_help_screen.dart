21a7979daa48e8ab235dd2461eda44fa02571ae7
Mustafa <ozdemir1794@hotmail.com>
2025-12-21 16:59:35 +0300

    chore: UI color changed

--- lib/Screens/get_help_screen.dart
+++ lib/Screens/get_help_screen.dart
@@ -568,6 +568,7 @@ class _GetHelpScreenState extends ConsumerState<GetHelpScreen> {
                       borderRadius: BorderRadius.circular(8),
                     ),
                     child: DropdownButtonFormField<String>(
+                      style: GoogleFonts.poppins(color: primary),
                       initialValue: _selectedDuration,
                       isExpanded: true,
                       decoration: const InputDecoration.collapsed(hintText: ''),
@@ -610,6 +611,7 @@ class _GetHelpScreenState extends ConsumerState<GetHelpScreen> {
                       borderRadius: BorderRadius.circular(8),
                     ),
                     child: DropdownButtonFormField<int>(
+                      style: GoogleFonts.poppins(color: primary),
                       initialValue: _peopleNeeded,
                       isExpanded: true,
                       decoration: const InputDecoration.collapsed(hintText: ''),
